d0dd3912a4e8a6f9d04177369857f0e069d2c278
kolodxxv <crowcrxst@gmail.com>
2023-06-02 17:11:08 +0300

    AFE-009.4 Fixed cyrillic letters error, added russian version of stepper.

--- AFE/src/app/utils/utils.ts
+++ AFE/src/app/utils/utils.ts
@@ -12,7 +12,7 @@ export class Utils {
     formParams.forEach(item => {
       this.generatedForm.addControl(`${item}Ctrl`, new FormControl('', [
           Validators.required,
-          Validators.pattern("[A-Za-z]*")
+          Validators.pattern("[A-Za-z\u0401\u0451\u0410-\u044f]*")
       ]));
     })
     return this.generatedForm;
